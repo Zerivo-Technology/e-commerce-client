72cb9111ad243025b0c71d42d0cb413f69413cc1
argf013 <alghi344@gmail.com>
2024-11-18 13:50:44 +0700

    init asset

---
+++ src/assets/images/index.ts
@@ -0,0 +1,23 @@
+export { default as header } from './header.png';
+
+export { default as producta } from './product-1.png';
+export { default as productc } from './product-2.png';
+export { default as productb } from './product-3.png';
+
+export { default as graphicA } from './graphic-design-1.jpg';
+export { default as graphicB } from './graphic-design-2.jpg';
+export { default as graphicC } from './graphic-design-3.jpg';
+export { default as graphicD } from './graphic-design-4.jpg';
+export { default as graphicE } from './graphic-design-5.jpg';
+export { default as graphicF } from './graphic-design-6.jpg';
+export { default as graphicG } from './graphic-design-7.jpg';
+export { default as graphicH } from './graphic-design-8.jpg';
+export { default as graphicI } from './graphic-design-9.jpg';
+export { default as graphicJ } from './graphic-design-10.jpg';
+
+export { default as featuredA } from './featured-1.jpg';
+export { default as featuredB } from './featured-2.jpg';
+
+export { default as subscribeBG } from './subscribe-bg.jpg';
+
+export { default as profile } from './profile.jpg';
