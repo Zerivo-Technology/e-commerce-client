0053bebe3953e1a42d935d47882ae57a6d75e856
argf013 <alghi344@gmail.com>
2024-11-18 13:51:48 +0700

    feat: login & register

--- src/routes/userRoot.tsx
+++ src/routes/userRoot.tsx
@@ -1,44 +1,52 @@
-import Loading from "@/components/element/Loading";
-import FooterUser from "@/components/user/Footer";
-import NavbarUser from "@/components/user/Navbar";
-import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loading from '@/components/element/Loading';
+import FooterUser from '@/components/user/Footer';
+import NavbarUser from '@/components/user/Navbar';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const PagesUser = lazy(() => import("@/pages/user"))
-const LoginUser = lazy(() => import("@/pages/user/auth/Login"))
+const PagesUser = lazy(() => import('@/pages/user'));
+const LoginUser = lazy(() => import('@/pages/user/auth/Login'));
 
 const UserRoot = () => {
-    return (
-        <Router>
-            <div className="min-h-screen h-max w-full flex flex-col">
-            {/* element Navbar */}
-            <NavbarUser />
-            {/* element Navbar */}
-            
-                <Routes>
-                    <Route 
-                        path="/"
-                        element={
-                            <Suspense fallback={<Loading />}>
-                                <PagesUser />
-                            </Suspense>
-                        }
-                    />
-                    <Route 
-                        path="/sign-in"
-                        element={
-                            <Suspense fallback={<Loading />}>
-                                <LoginUser />
-                            </Suspense>
-                        }
-                    />
-                </Routes>
+  return (
+    <Router>
+      <div className="min-h-screen h-max w-full flex flex-col">
+        {/* element Navbar */}
+        <NavbarUser />
+        {/* element Navbar */}
 
-            {/* Element Footer */}
-            <FooterUser />
-            </div>
-        </Router>
-    )
-}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <PagesUser />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/sign-in"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LoginUser mode="login" />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LoginUser mode="registration" />
+              </Suspense>
+            }
+          />
+        </Routes>
 
-export default UserRoot
+        {/* Element Footer */}
+        <FooterUser />
+      </div>
+    </Router>
+  );
+};
+
+export default UserRoot;
