0eed1801e33202a701a37c93206447103718060e
argf013 <alghi344@gmail.com>
2024-11-22 14:32:48 +0700

    refactor: init cart totals component

--- src/pages/user/shop/Cart.tsx
+++ src/pages/user/shop/Cart.tsx
@@ -1,8 +1,9 @@
 import Button from '@/components/element/Button';
+import CartTotals from '@/components/element/CartTotals';
 import Input from '@/components/element/Input';
 import { faClose } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface CartItem {
   id: number;
@@ -36,8 +37,10 @@ const cartItems: CartItem[] = [
 ];
 
 const Cart: React.FC = () => {
-  const [rangeValue, setRangeValue] = useState(0);
-  const [inputCode, setInputCode] = useState('');
+  const [rangeValue, setRangeValue] = useState<number>(1);
+  const [inputCode, setInputCode] = useState<string>('');
+
+  const navigate = useNavigate();
 
   const handleRangeChange = (value: number | string) => {
     setRangeValue(value as number);
@@ -45,6 +48,14 @@ const Cart: React.FC = () => {
 
   const handleInputChange = (value: number | string) => {
     setInputCode(value as string);
+  };
+
+  const handleProceedToCheckout = () => {
+    navigate('/delivery-payment');
+  };
+
+  const handleApplyDiscount = () => {
+    console.log('Discount applied', inputCode);
   };
 
   return (
@@ -94,15 +105,11 @@ const Cart: React.FC = () => {
                 <span className="font-medium w-[72px] text-center">
                   ${item.price}
                 </span>
-                <span
-                  id="delete-button"
-                  className="border border-primary-color h-10 w-10 rounded-full flex items-center justify-center"
-                >
-                  <FontAwesomeIcon
-                    icon={faClose}
-                    className="text-primary-color"
-                  />
-                </span>
+                <Button
+                  className="border border-primary-color hover:bg-primary-color h-10 w-10 rounded-full flex items-center justify-center"
+                  icon={faClose}
+                  label=""
+                />
               </div>
             ))}
           </div>
@@ -110,39 +117,18 @@ const Cart: React.FC = () => {
       </div>
       <div className="flex font-monserat container mx-auto py-12">
         <div className="ml-auto">
-          <div className="flex flex-col w-[439px] gap-5 text-dark-background">
-            <h3 className="text-dark-background font-bold text-2xl">
-              CART TOTALS
-            </h3>
-            <span id="separator" className="border border-[#FE7E7E7]" />
-            <Input
-              type="text"
-              value={inputCode}
-              onChange={handleInputChange}
-              width="100%"
-              useButton
-              placeholder="Input voucher discount code"
-            />
-            <div className="flex flex-col">
-              <div className="flex justify-between border-b py-2">
-                <p>Subtotal</p>
-                <p>100</p>
-              </div>
-              <div className="flex justify-between border-b py-2">
-                <p>Discount</p>
-                <p>0</p>
-              </div>
-              <div className="flex justify-between border-b py-2">
-                <p>Shipping free</p>
-                <p>0</p>
-              </div>
-              <div className="flex justify-between border-b py-2 font-bold">
-                <p>Total</p>
-                <p>$100</p>
-              </div>
-            </div>
-            <Button label="Proceed To Checkout" severity="primary" className='w-fit px-4 rounded-md font-bold ml-auto' />
-          </div>
+          <CartTotals
+            handleApplyDiscount={handleApplyDiscount}
+            totals={{
+              subTotal: 50,
+              discount: 0,
+              shippingFee: 10,
+              total: 60,
+            }}
+            inputCode={inputCode}
+            handleInputChange={handleInputChange}
+            handleProceedToCheckout={handleProceedToCheckout}
+          />
         </div>
       </div>
     </div>
